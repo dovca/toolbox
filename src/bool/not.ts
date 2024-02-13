8fbfdadb9a92aec95fbde5000d86eb19d2fe27e1
David Ovčačík <david.ovcacik@gmail.com>
2024-02-13 23:21:23 +0100

    Improve 'not' overloads

--- src/bool/not.ts
+++ src/bool/not.ts
@@ -1,5 +1,6 @@
 export function not(input: false | '' | 0 | null | undefined | 0n): true;
 export function not(input: true | 1): false;
+export function not(input: boolean): boolean;
 export function not(input: unknown): boolean;
 export function not(input: any): boolean {
 	return !input;
