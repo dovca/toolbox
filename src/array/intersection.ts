4714b39dc231c84d6c11694257a1ec108420f34a
David Ovčačík <david.ovcacik@gmail.com>
2024-02-24 21:29:31 +0100

    Improve intersection functions

--- src/array/intersection.ts
+++ src/array/intersection.ts
@@ -1,10 +1,17 @@
 import type {Equalizer, Fn} from '../types';
 import {isOneOf} from '../predicate';
+import {memoize} from '../function';
 
 export function intersection<T>(other: ReadonlyArray<T>): Fn<T[], ReadonlyArray<T>> {
 	return (input) => input.filter(isOneOf(...other));
 }
 
 export function intersectionWith<T>(comparator: Equalizer<T>): Fn<Fn<T[], ReadonlyArray<T>>, ReadonlyArray<T>> {
-	return (other) => (input) => input.filter((i) => other.some((o) => comparator(i, o)));
+	const memComparator = memoize(comparator);
+	return (other) => (input) => input.filter((i) => other.some((o) => memComparator(i, o)));
+}
+
+export function intersectionBy<T, M>(mapper: Fn<M, T>): Fn<Fn<T[], ReadonlyArray<T>>, ReadonlyArray<T>> {
+	const memMapper = memoize(mapper);
+	return intersectionWith((i, o) => memMapper(i) === memMapper(o));
 }
--- test/array/intersection.test.ts
+++ test/array/intersection.test.ts
@@ -1,14 +1,34 @@
 import {expect, test} from 'bun:test';
-import {intersection, intersectionWith} from '../../src';
+import {intersection, intersectionBy, intersectionWith} from '../../src';
 
 test('intersection', () => {
 	expect(intersection([])([])).toEqual([]);
+	expect(intersection([1])([])).toEqual([]);
+	expect(intersection<number>([])([1])).toEqual([]);
 	expect(intersection([1, 2, 3])([4, 5, 6])).toEqual([]);
 	expect(intersection([1, 2, 3])([3, 4, 5])).toEqual([3]);
+	expect(intersection([1, 2, 3])([3, 2, 1])).toEqual([3, 2, 1]);
 });
 
 test('intersectionWith', () => {
 	const equalLength = (a: string, b: string) => a.length === b.length
-	expect(intersectionWith(equalLength)([])([])).toEqual([]);
-	expect(intersectionWith(equalLength)(['a', 'aa', 'aaa'])(['dddd', 'ccc', 'bb'])).toEqual(['ccc', 'bb']);
+	const intersect = intersectionWith(equalLength);
+
+	expect(intersect([])([])).toEqual([]);
+	expect(intersect(['a'])([])).toEqual([]);
+	expect(intersect([])(['a'])).toEqual([]);
+	expect(intersect(['a'])(['b'])).toEqual(['b']);
+	expect(intersect(['a', 'aa', 'aaa'])(['dddd', 'ccc', 'bb'])).toEqual(['ccc', 'bb']);
+});
+
+test('intersectionBy', () => {
+	const firstChar = (s: string) => s.charAt(0);
+	const intersect = intersectionBy(firstChar);
+
+	expect(intersect([])([])).toEqual([]);
+	expect(intersect(['a'])([])).toEqual([]);
+	expect(intersect([])(['a'])).toEqual([]);
+	expect(intersect(['a'])(['b'])).toEqual([]);
+	expect(intersect(['a', 'aa', 'aaa'])(['dddd', 'ccc', 'bb'])).toEqual([]);
+	expect(intersect(['a', 'dd', 'eee'])(['ccc', 'bb', 'a', 'aa'])).toEqual(['a', 'aa']);
 });
