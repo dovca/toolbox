4ebe1d42c8639f517dc4f989f157b9576dcb3edd
David Ovčačík <david.ovcacik@gmail.com>
2024-06-30 00:19:40 +0200

    Add more docs to some functions

--- src/function/gather.ts
+++ src/function/gather.ts
@@ -1,24 +1,120 @@
 import type {Fn, Fn2, Fn3, Fn4, Fn5} from '../types/utils';
 import {identity} from '../misc/identity';
 
-export function gather(): Fn<[], void>;
-export function gather<A>(): Fn<[A], A>;
-export function gather<A, B>(): Fn2<[A, B], A, B>;
-export function gather<A, B, C>(): Fn3<[A, B, C], A, B, C>;
-export function gather<A, B, C, D>(): Fn4<[A, B, C, D], A, B, C, D>;
-export function gather<A, B, C, D, E>(): Fn5<[A, B, C, D, E], A, B, C, D, E>;
-export function gather<R, A>(fn: Fn<R, [A]>): Fn<R, A>;
-export function gather<R, A, B>(fn: Fn<R, [A, B]>): Fn2<R, A, B>;
-export function gather<R, A, B, C>(fn: Fn<R, [A, B, C]>): Fn3<R, A, B, C>;
-export function gather<R, A, B, C, D>(fn: Fn<R, [A, B, C, D]>): Fn4<R, A, B, C, D>;
-export function gather<R, A, B, C, D, E>(fn: Fn<R, [A, B, C, D, E]>): Fn5<R, A, B, C, D, E>;
-export function gather<R>(fn?: (args: any[]) => R): Fn<R, any[]>;
-
 /**
  * Gathers a set of arguments and passes them to a function as a tuple.
- * @param fn The function to gather the arguments for. Defaults to the identity function.
- * @returns The result of the function.
+ * @returns In this form with no function and arguments given, it produces an empty array.
+ * @example
+ * ```typescript
+ * gather()(); // []
+ * ```
  */
+export function gather(): Fn<[], void>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @returns In this form with no function given, it produces the single argument as an array.
+ * @example
+ * ```typescript
+ * gather()(1); // [1]
+ * ```
+ */
+export function gather<A>(): Fn<[A], A>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @returns In this form with no function given, it produces the two arguments as an array.
+ * @example
+ * ```typescript
+ * gather()(1, 2); // [1, 2]
+ * ```
+ */
+export function gather<A, B>(): Fn2<[A, B], A, B>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @returns In this form with no function given, it produces the three arguments as an array.
+ * @example
+ * ```typescript
+ * gather()(1, 2, 3); // [1, 2, 3]
+ * ```
+ */
+export function gather<A, B, C>(): Fn3<[A, B, C], A, B, C>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @returns In this form with no function given, it produces the four arguments as an array.
+ * @example
+ * ```typescript
+ * gather()(1, 2, 3, 4); // [1, 2, 3, 4]
+ * ```
+ */
+export function gather<A, B, C, D>(): Fn4<[A, B, C, D], A, B, C, D>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @returns In this form with no function given, it produces the five arguments as an array.
+ * @example
+ * ```typescript
+ * gather()(1, 2, 3, 4, 5); // [1, 2, 3, 4, 5]
+ * ```
+ */
+export function gather<A, B, C, D, E>(): Fn5<[A, B, C, D, E], A, B, C, D, E>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @param fn The function to call with the single argument as an array.
+ * @returns Produces the result of the function.
+ * @example
+ * ```typescript
+ * gather(sum)(1); // 1
+ * ```
+ */
+export function gather<R, A>(fn: Fn<R, [A]>): Fn<R, A>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @param fn The function to call with the two gathered arguments.
+ * @returns Produces the result of the function.
+ * @example
+ * ```typescript
+ * gather(sum)(1, 2); // 3 as in sum([1, 2])
+ * ```
+ */
+export function gather<R, A, B>(fn: Fn<R, [A, B]>): Fn2<R, A, B>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @param fn The function to call with the three gathered arguments.
+ * @returns Produces the result of the function.
+ * @example
+ * ```typescript
+ * gather(sum)(1, 2, 3); // 6 as in sum([1, 2, 3])
+ * ```
+ */
+export function gather<R, A, B, C>(fn: Fn<R, [A, B, C]>): Fn3<R, A, B, C>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @param fn The function to call with the four gathered arguments.
+ * @returns Produces the result of the function.
+ * @example
+ * ```typescript
+ * gather(sum)(1, 2, 3, 4); // 10 as in sum([1, 2, 3, 4])
+ * ```
+ */
+export function gather<R, A, B, C, D>(fn: Fn<R, [A, B, C, D]>): Fn4<R, A, B, C, D>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @param fn The function to call with the five gathered arguments.
+ * @returns Produces the result of the function.
+ * @example
+ * ```typescript
+ * gather(sum)(1, 2, 3, 4, 5); // 15 as in sum([1, 2, 3, 4, 5])
+ * ```
+ */
+export function gather<R, A, B, C, D, E>(fn: Fn<R, [A, B, C, D, E]>): Fn5<R, A, B, C, D, E>;
+/**
+ * Gathers a set of arguments and passes them to a function as a tuple.
+ * @param fn The function to call with the gathered arguments.
+ * @returns Produces the result of the function.
+ * @example
+ * ```typescript
+ * gather(sum)(1, 2, 3, 4, 5); // 15 as in sum([1, 2, 3, 4, 5])
+ * ```
+ */
+export function gather<R>(fn?: (args: any[]) => R): Fn<R, any[]>;
 export function gather<R>(fn: Fn<R, any> = identity): (...args: any[]) => R {
 	return (...args) => fn(args);
 }
--- src/multi/slice.ts
+++ src/multi/slice.ts
@@ -1,5 +1,5 @@
 import {type Slice, slice as typedSlice} from 'string-ts';
-import type {Fn, Negative} from '../types/utils';
+import type {AnyArray, Fn, Negative} from '../types/utils';
 import type {First, Shift} from '../types/array';
 import {isArray} from '../predicate/isArray';
 
@@ -11,10 +11,49 @@ function stringSlice<T extends string, S extends number = 0, E extends number = 
 	return (input) => typedSlice(input, start, end);
 }
 
+/**
+ * Produces a copy of the input string by slicing it from start to end. All characters are included.
+ * @returns Produces the same string.
+ * @example
+ * ```typescript
+ * slice()('hello'); // 'hello'
+ * ```
+ */
 export function slice<T extends string>(): Fn<Slice<T, 0, -1>, T>;
+/**
+ * Produces a new string by slicing the input string from `start` onwards.
+ * @param start The index to start slicing from. 0 corresponds to the start of the string.
+ * @returns Produces a new string.
+ * @example
+ * ```typescript
+ * slice(3)('hello'); // 'lo'
+ * ```
+ */
 export function slice<T extends string, S extends number>(start: S): Fn<Slice<T, S, -1>, T>;
+/**
+ * Produces a new string by slicing the input string from `start` to `end`.
+ * @param start The index to start slicing from. 0 corresponds to the start of the string.
+ * @param end The index to stop slicing at. -1 corresponds to the end of the string.
+ * @returns Produces a new string.
+ * @example
+ * ```typescript
+ * slice(1, 4)('hello'); // 'ell'
+ * slice(1, -2)('hello'); // 'ell'
+ * ```
+ */
 export function slice<T extends string, S extends number, E extends number>(start: S, end: E): Fn<Slice<T, S, E>, T>;
-export function slice<T extends readonly any[]>(start?: number, end?: number): Fn<T>;
+/**
+ * Produces a new array by slicing the input array from `start` to `end`.
+ * @param start The index to start slicing from. 0 corresponds to the start of the array.
+ * @param end The index to stop slicing at. -1 corresponds to the end of the array.
+ * @returns Produces a new array.
+ * @example
+ * ```typescript
+ * slice(1, 4)([1, 2, 3, 4, 5]); // [2, 3, 4]
+ * slice(1, -2)([1, 2, 3, 4, 5]); // [2, 3]
+ * ```
+ */
+export function slice<T extends AnyArray>(start?: number, end?: number): Fn<T>;
 export function slice(start?: number, end?: number): Fn<unknown[]> | Fn<string> {
 	return ((input: unknown) => {
 		if (typeof input === 'string') {
@@ -27,19 +66,73 @@ export function slice(start?: number, end?: number): Fn<unknown[]> | Fn<string> 
 	}) as Fn<unknown[], readonly unknown[]> | Fn<string>;
 }
 
+/**
+ * Produces a new string that contains the first `count` characters of the input string.
+ * @param count The number of characters to include from the start of the string. Defaults to 1.
+ * @returns Produces a new string.
+ * @example
+ * ```typescript
+ * head(3)('hello'); // 'hel'
+ * ```
+ */
 export function head<T extends string, N extends number>(count?: N): Fn<Slice<T, 0, N>, T>;
+/**
+ * Produces a new array that contains the first `count` elements of the input array.
+ * @param count The number of elements to include from the start of the array. Defaults to 1.
+ * @returns Produces a new array.
+ * @example
+ * ```typescript
+ * head(3)([1, 2, 3, 4, 5]); // [1, 2, 3]
+ * ```
+ */
 export function head<T extends readonly any[]>(count?: number): Fn<T>;
 export function head(count: number = 1): Fn<string> | Fn<unknown[]> {
 	return (input: any) => slice(0, count)(input);
 }
 
+/**
+ * Produces a new string that contains the last `count` characters of the input string.
+ * @param count The number of characters to include from the end of the string. Defaults to 1.
+ * @returns Produces a new string.
+ * @example
+ * ```typescript
+ * tail(3)('hello'); // 'llo'
+ * ```
+ */
 export function tail<T extends string, N extends number>(count?: N): Fn<Slice<T, Negative<N>>, T>;
+/**
+ * Produces a new array that contains the last `count` elements of the input array.
+ * @param count The number of elements to include from the end of the array. Defaults to 1.
+ * @returns Produces a new array.
+ * @example
+ * ```typescript
+ * tail(3)([1, 2, 3, 4, 5]); // [3, 4, 5]
+ * ```
+ */
 export function tail<T extends readonly any[]>(count?: number): Fn<T>;
 export function tail(count: number = 1): Fn<string> | Fn<unknown[]> {
 	return (input: any) => slice(-count)(input);
 }
 
+/**
+ * Returns the first character and the rest of a string as a tuple resembling the removed "head" and the rest.
+ * @param value The string to decapitate.
+ * @returns The separated head and the rest of the string.
+ * @example
+ * ```typescript
+ * decapitate('hello'); // ['h', 'ello']
+ * ```
+ */
 export function decapitate<T extends string>(value: T): [Slice<T, 0, 1>, Slice<T, 1>];
+/**
+ * Returns the first element and the rest of an array as a tuple resembling the removed "head" and the rest.
+ * @param values The array to decapitate.
+ * @returns The separated head and the rest of the array.
+ * @example
+ * ```typescript
+ * decapitate([1, 2, 3]); // [1, [2, 3]]
+ * ```
+ */
 export function decapitate<T extends readonly any[]>(values: T): [First<T>, Shift<T>];
 export function decapitate<T extends string | readonly any[]>(values: T): [string, string] | [any, any[]] {
 	return typeof values === 'string'
