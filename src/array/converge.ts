2f1b77a009c983d05f67f731f52d8a88176738b0
David Ovčačík <david.ovcacik@gmail.com>
2024-08-19 13:25:40 +0200

    Optimize converge function

--- src/array/converge.ts
+++ src/array/converge.ts
@@ -1,24 +1,19 @@
 import {identity} from '../misc/identity';
-import type {Numeric, Fn, Maybe, Sorter} from '../types/utils';
+import type {Numeric, Fn, Maybe, Sorter, IterationCallback} from '../types/utils';
+import {forwardIterator} from '../iterators/forward';
 
 export function converge<Input, Mapped = Input>(
 	comparator: Sorter<Mapped>,
-	mapper: Fn<Mapped, Input> = identity,
+	mapper: IterationCallback<Mapped, Input> = identity,
 ): Fn<Maybe<Input>, readonly Input[]> {
 	return (values) => {
 		let best: Maybe<Input> = undefined;
 		let mappedBest: Maybe<Mapped> = undefined;
 
-		for (const value of values) {
-			if (best === undefined) {
-				best = value;
-				mappedBest = mapper(value);
-				continue;
-			}
+		for (const [value, index, array] of forwardIterator(values)) {
+			const mappedValue = mapper(value, index, array);
 
-			const mappedValue = mapper(value);
-
-			if (comparator(mappedBest!, mappedValue) < 0) {
+			if (index === 0 || comparator(mappedBest!, mappedValue) < 0) {
 				best = value;
 				mappedBest = mappedValue;
 			}
@@ -29,13 +24,13 @@ export function converge<Input, Mapped = Input>(
 }
 
 export function minimize(): Fn<Maybe<Numeric>, readonly Numeric[]>;
-export function minimize<T>(mapper: Fn<Numeric, T>): Fn<Maybe<T>, readonly T[]>;
-export function minimize<T>(mapper: Fn<Numeric, T> = identity): Fn<Maybe<T>, readonly T[]> {
+export function minimize<T>(mapper: IterationCallback<Numeric, T>): Fn<Maybe<T>, readonly T[]>;
+export function minimize<T>(mapper: IterationCallback<Numeric, T> = identity): Fn<Maybe<T>, readonly T[]> {
 	return converge<T, Numeric>((a, b) => b.valueOf() - a.valueOf(), mapper);
 }
 
 export function maximize(): Fn<Maybe<Numeric>, readonly Numeric[]>;
-export function maximize<T>(mapper: Fn<Numeric, T>): Fn<Maybe<T>, readonly T[]>;
-export function maximize<T>(mapper: Fn<Numeric, T> = identity): Fn<Maybe<T>, readonly T[]> {
+export function maximize<T>(mapper: IterationCallback<Numeric, T>): Fn<Maybe<T>, readonly T[]>;
+export function maximize<T>(mapper: IterationCallback<Numeric, T> = identity): Fn<Maybe<T>, readonly T[]> {
 	return converge<T, Numeric>((a, b) => a.valueOf() - b.valueOf(), mapper);
 }
