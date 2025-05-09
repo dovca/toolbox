bd5a81691788fe9454d11d65a6786cdee9a0a2a1
David Ovčačík <david.ovcacik@gmail.com>
2025-05-09 22:24:16 +0200

    Improve mapValues function typing and docs

--- src/object/mapValues.ts
+++ src/object/mapValues.ts
@@ -1,25 +1,47 @@
-import type {Dictionary, Fn, Fn2, Indexable, StringKeys, ValidIndex, Values} from '../types/utils';
+import type {Fn2, Indexable, StringKeys, ValidIndex, Values} from '../types/utils';
 import {entries} from './entries';
+import type {ToString} from '../types/convert';
 
-/** Transforms the values of an object.
- * @param mapper The function that maps the values.
+/** `mapValues<ValueIn, KeyIn, ValueOut>`
+ *
+ * Transforms the values of an object while keeping keys intact.
+ * @param `mapper` The function that maps the values.
  * @returns Produces a new object with the mapped values.
+ * @generics The generic params are:
+ * - `ValueIn` The type of the flowing object's values.
+ * - `KeyIn` The type of the flowing object's keys.
+ * - `ValueOut` The type of the mapper function's output.
  * @example
  * ```typescript
  * mapValues((value) => value * 2)({a: 1, b: 2}); // {a: 2, b: 4}
  * ```
  */
-export function mapValues<ValueIn, ValueOut>(mapper: Fn2<ValueOut, ValueIn, ValidIndex>): <Obj extends Dictionary<ValueIn>>(object: Obj) => { [K in keyof Obj]: ValueOut };
-export function mapValues<
-	ObjIn extends Indexable,
-	ObjOut extends { [k in StringKeys<ObjIn>]: any},
->(mapper: Fn2<ObjOut[StringKeys<ObjIn>], Values<ObjIn>, StringKeys<ObjIn>>): Fn<ObjOut, ObjIn>;
-export function mapValues<
-	I extends Indexable,
-	O extends { [k in StringKeys<I>]: any},
->(mapper: Fn2<O[StringKeys<I>], Values<I>, StringKeys<I>>): Fn<O, I> {
-	return (obj) => {
-		const result = {} as O;
+export function mapValues<ValueIn, KeyIn extends ValidIndex, ValueOut>(
+	mapper: Fn2<ValueOut, ValueIn, ToString<KeyIn>>,
+): (object: Record<KeyIn, ValueIn>) => Record<ToString<KeyIn>, ValueOut>;
+
+/** `mapValues<ObjectIn, ValueOut>`
+ *
+ * Transforms the values of an object while keeping keys intact.
+ * @param `mapper` The function that maps the values.
+ * @returns Produces a new object with the mapped values.
+ * @generics The generic params are:
+ * - `ObjectIn` The type of the flowing object
+ * - `ValueOut` The type of the mapper function's output.
+ * @example
+ * ```typescript
+ * mapValues((value) => value * 2)({a: 1, b: 2}); // {a: 2, b: 4}
+ * ```
+ */
+export function mapValues<ObjectIn extends Indexable, ValueOut>(
+	mapper: Fn2<ValueOut, Values<ObjectIn>, StringKeys<ObjectIn>>,
+): (object: ObjectIn) => Record<StringKeys<ObjectIn>, ValueOut>;
+
+export function mapValues<ValueIn, KeyIn extends ValidIndex, ValueOut>(
+	mapper: Fn2<ValueOut, ValueIn, ToString<KeyIn>>,
+): (object: Record<KeyIn, ValueIn>) => Record<ToString<KeyIn>, ValueOut> {
+	return (obj: { [K in KeyIn]: ValueIn}) => {
+		const result = {} as { [K in ToString<KeyIn>]: ValueOut };
 		for (const [key, value] of entries(obj)) {
 			result[key] = mapper(value, key);
 		}
--- test/object/mapValues.test.ts
+++ test/object/mapValues.test.ts
@@ -1,9 +1,11 @@
 import {expect, test} from 'bun:test';
-import {double, mapValues} from '../../src';
+import {double, flow, mapValues} from '../../src';
 
 test('mapValues', () => {
-	const obj = {a: 1, b: 2};
+	const obj = {a: 1, b: 2} as const;
 
 	expect(mapValues(double)({})).toEqual({});
 	expect(mapValues(double)(obj)).toEqual({a: 2, b: 4});
+	expect(flow(obj, mapValues((value, key) => `${key}:${value}`))).toEqual({a: 'a:1', b: 'b:2'});
+	expect(mapValues<typeof obj, string>((value, key) => `${key}:${value}`)(obj)).toEqual({a: 'a:1', b: 'b:2'});
 });
